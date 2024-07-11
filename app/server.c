2f677ee778864198c83e23f931a5bd3d85844468
smikmani <smikmani@hotmail.com>
2024-07-11 23:34:56 +0300

    res

--- app/server.c
+++ app/server.c
@@ -28,7 +28,7 @@ int main() {
 	
 	// // Since the tester restarts your program quite often, setting SO_REUSEADDR
 	// // ensures that we don't run into 'Address already in use' errors
-	
+
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
 		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
@@ -54,9 +54,11 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int conn_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
-	
+	char* res = "HTTP/1.1 200 OK\r\n\r\n";
+
+	write(conn_fd,&res,sizeof(res));
 	close(server_fd);
 
 	return 0;
